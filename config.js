77bf958a5b66f21030d07d2bc080551db7bcc056
whatsup10 <66445098+whatsup10@users.noreply.github.com>
2025-08-04 20:27:29 -0400

    Added more styling

--- config.js
+++ config.js
@@ -1,12 +1,6 @@
-from dotenv import load_dotenv
-import os
-load_dotenv()
-
-token = os.getenv('token')
-
 var config = {
     style: 'mapbox://styles/tgnewikow/cmdnimw9n001b01sg61y7a5hy',
-    accessToken: token,
+    accessToken: 'pk.eyJ1IjoidGduZXdpa293IiwiYSI6ImNseXl1NHVydDA4Ym8yam10MXBobTZsYmkifQ.bYbynMzR-x5d1NHr3tOICw',
     showMarkers: false,
     markerColor: '#3FB1CE',
     //projection: 'equirectangular',
@@ -23,7 +17,7 @@ var config = {
     title: 'How are California high schools named?',
     subtitle: 'Some common themes arise for schools in the Golden State.',
     byline: 'By Tara Gnewikow',
-    footer: 'Source: California Department of Education <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
+    footer: 'Source: California Department of Education <br> You can find the <a href="https://github.com/tgnewikow/CA-high-school-names" target="_blank">GitHub repository for this analysis here</a>.',
     chapters: [
         {
             id: 'all-schools',
@@ -60,7 +54,7 @@ var config = {
             id: 'place-names',
             alignment: 'left',
             hidden: false,
-            title: 'About three in five schools are named after a place, most commonly the city the school is in.',
+            title: 'About three in five schools are named after a <span style="color:#70b44b;">place</span>, most commonly the city the school is in.',
             image: './assets/berkeley-sign.png',
             //description: '',
             location: {
@@ -91,7 +85,7 @@ var config = {
             id: 'person-names',
             alignment: 'left',
             hidden: false,
-            title: 'Only about 17 percent of schools are named after a person.',
+            title: 'Only about 17 percent of schools are named after a <span style="color:#8f4bb4;">person</span>.',
             image: './assets/jfk-drawing.png',
             //description: '',
             location: {
@@ -122,8 +116,8 @@ var config = {
             id: 'other-names',
             alignment: 'left',
             hidden: false,
-            title: 'And about one in four schools are named after something else.',
-            //description: '',
+            title: 'And about one in four schools are named after <span style="color:#4ba4b4;">something else</span>.',
+            image: './assets/tree-art.png',
             location: {
                 center: [-123.45156, 37.95943],
                 zoom: 4.78,
@@ -152,7 +146,7 @@ var config = {
             id: 'spanish-names',
             alignment: 'left',
             hidden: false,
-            title: 'Almost one-fourth of schools have Spanish names.',
+            title: 'Almost one-fourth of schools have <span style="color:#b45b4b;">Spanish</span> names.',
             //description: '',
             location: {
                 center: [-123.45156, 37.95943],
@@ -174,6 +168,36 @@ var config = {
                 {
                      layer: 'Spanish names',
                      opacity: 0,
+                     duration: 500
+                }
+            ]
+        },
+        {
+            id: 'spanish-names-la',
+            alignment: 'left',
+            hidden: false,
+            title: 'Los Angeles County has the most schools with Spanish names.',
+            //description: '',
+            location: {
+                center: [-118.51414, 34.18457],
+                zoom: 7.80,
+                pitch: 0,
+                bearing: 0,
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+                {
+                     layer: 'Spanish names',
+                     opacity: 1,
+                     duration: 500
+                }
+            ],
+            onChapterExit: [
+                {
+                     layer: 'Spanish names',
+                     opacity: 1,
                      duration: 500
                 }
             ]
